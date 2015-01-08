7ee0ae76a0675c953877ba878c1b62fcd07740cc
ZielonyKazik <dawidszczyrk@gmail.com>
2015-01-08 02:27:12 +0100

    first

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewData["Colors"] = new List<string>
+            {
+                "Blue",
+                "Green",
+                "Red"
+            };
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
